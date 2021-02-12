c4fb252bc1249e5ba7f9f021a46315898b0070c5
Lourdespc <loprico88@gmail.com>
2021-02-12 10:41:37 +0100

    Cambios varios

---
+++ decide/examen/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/home/fran/Escritorio/examen/EGC-1230-loupricor/decide/examen/bin/python3
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
